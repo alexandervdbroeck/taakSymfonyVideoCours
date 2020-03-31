93f3553a85292433619afb1530a09f72420ebf7e
Elyoneondra <hannelore.lmtt@gmail.com>
2020-03-31 11:32:49 +0200

    connection to ConnectionInfo class

---
+++ src/Controller/ConnectionInfo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+class ConnectionInfo {
+
+     public function connectionInfo() {
+          return '"mysql:host=185.115.218.166;dbname=wdev_alexander", "wdev_alexander", "u2k8EwwQvDav"';
+     }
+}
