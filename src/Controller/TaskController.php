c00cc46dd46bb6dbf4de0fae97e7942515caf608
alexander <alexander.vdbroeck@gmail.com>
2020-04-01 22:41:58 +0200

    api taken

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -18,17 +18,19 @@ class TaskController extends AbstractController
      {
         $cont = new Container();
         $this->taskService = $cont->getTaskLoader();
+         return new JsonResponse("succes");
 
      }
 
-     //        -        -        -        G E T   A L L   T A S K S        -        -        -
-        /**
-     * @Route("/api/taken", methods={"GET"})
+
+    //        -        -        -        D E L E T E   1   T A S K   B Y   I D        -        -        -
+    /**
+     * @Route("/api/taak/{taskid}", methods={"DELETE"})
      */
-    public function getAllTasks()
+    public function deleteOnelTasks( $taskid )
     {
-         $this->taskService->procesApiGetAllTasks();
-        return new JsonResponse("succes");
+        $this->taskService->procesApiDeleteTaskById($taskid);
+        return new JsonResponse("respons");
     }
 
      //        -        -        -        G E T   1   T A S K   B Y   I D        -        -        -
@@ -37,57 +39,40 @@ class TaskController extends AbstractController
      */
     public function getOneTasks( $taskid )
     {
-     $task = $this->getData( 'SELECT * FROM taak where taa_id =' . $taskid );
+        $this->taskService->procesApiGetTaskById($taskid);
+        return new JsonResponse("respons");
+    }
 
-        return $this->json( $task[0] );
+    //        -        -        -        G E T   A L L   T A S K S        -        -        -
+    /**
+     * @Route("/api/taken", methods={"GET"})
+     */
+    public function getAllTasks()
+    {
+        $this->taskService->procesApiGetAllTasks();
+        return new JsonResponse("succes");
     }
 
      //        -        -        -        A D D   1   T A S K        -        -        -
         /**
-     * @Route("/api/taak/", methods={"POST"})
+     * @Route("/api/taken/", methods={"POST"})
      */
     public function addOnelTasks()
     {
-     $task = file_get_contents('php://input');
-     $task= json_decode($task, true);
-     $taskDate = htmlentities($task[ 'taa_datum' ], ENT_QUOTES);
-     $taskDescr = htmlentities($task[ 'taa_omschr' ], ENT_QUOTES);
-
-     $sql = "INSERT INTO taak SET taa_datum = '" . $taskDate. "', taa_omschr = '" . $taskDescr. "'";
-
-     return $this->executeSQL($sql);
+        $this->taskService->procesApiCreateNewtask();
     }
 
      //        -        -        -        E D I T   1   T A S K   B Y   I D        -        -        -
         /**
-     * @Route("/api/taak/{taskid}", methods={"PUT"})
+     * @Route("/api/taken/{taskid}", methods={"PUT"})
      */
     public function editOnelTasks( $taskid )
     {
-        $this->taskService->procesApiGetTaskById($taskid);
+        $this->taskService->procesApiUpdateTaskById($taskid);
         return new JsonResponse("succes");
     }
 
 
 
-
-
-
-
-
-
-
-
-
-     //        -        -        -        D E L E T E   1   T A S K   B Y   I D        -        -        -
-        /**
-     * @Route("/api/taak/{taskid}", methods={"DELETE"})
-     */
-    public function deleteOnelTasks( $taskid )
-    {
-        // TODO - get the database response back
-
-        return $this->json( [ 'test' => 'this is my little test'] );
-    }
 
 }
